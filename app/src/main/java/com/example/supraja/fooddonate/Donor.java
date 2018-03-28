ecf2bafa809afcee2957b2184efb964dbe50717e
Supraja <supraja.b.2015.cse@rajalakshmi.edu.in>
2018-03-28 21:06:30 +0530

    register and details

---
+++ app/src/main/java/com/example/supraja/fooddonate/Donor.java
@@ -0,0 +1,8 @@
+package com.example.supraja.fooddonate;
+
+/**
+ * Created by Supraja on 3/28/2018.
+ */
+
+public class Donor {
+}
--- app/src/main/java/com/example/supraja/fooddonate/Retrieve.java
+++ app/src/main/java/com/example/supraja/fooddonate/Retrieve.java
@@ -1,13 +1,110 @@
 package com.example.supraja.fooddonate;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Retrieve extends AppCompatActivity {
+    ListView listView;
+    FirebaseDatabase database;
+    DatabaseReference ref;
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
+    Donor donor;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retrieve);
+        mAuth = FirebaseAuth.getInstance();
+
+        donor = new Donor();
+        listView = (ListView) findViewById(R.id.listView);
+        database = FirebaseDatabase.getInstance();
+        ref = database.getReference("Donor");
+
+        list = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(this, R.layout.user_info, R.id.userInfo, list);
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    donor = ds.getValue(Donor.class);
+                    list.add(donor.getName() + " " + donor.getContact() + " " + donor.getBloodgroup());
+                    listView.setAdapter(adapter);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
+
+    private void Logout() {
+        mAuth.signOut();
+        finish();
+        startActivity(new Intent(Retrieve.this, MainActivity.class));
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logoutMenu: {
+                Logout();
+                break;
+            }
+            case R.id.donateblood: {
+                startActivity(new Intent(Retrieve.this, Detail.class));
+                break;
+
+            }
+            case R.id.need: {
+                startActivity(new Intent(Retrieve.this, Retrieve.class));
+                break;
+
+
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
 }
